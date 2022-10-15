cb774b8441fbd45218e37a09830975935cacd837
Dinasi4ka <viychukdiana@gmail.com>
2022-10-15 16:44:34 +0300

    commit 1

---
+++ Lab04_1/Lab04_1.cpp
@@ -0,0 +1,51 @@
+// Lab04_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Війчук Діана
+//Лабораторна робота 4.1
+// Цикли
+// Варіант 2
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int k, N;
+    double S;
+
+    cout << "N = "; cin >> N;
+
+    S = 0;
+    k = N;
+    while (k <= 19)
+    {
+        S += sqrt(pow(sin(k), 2) + pow(cos(N / k), 2));
+        k++;
+    }
+    cout << S << endl;
+
+    S = 0;
+    k = N;
+    do {
+        S += sqrt(pow(sin(k), 2) + pow(cos(N / k), 2));
+        k++;
+    } while (k <= 19);
+    cout << S << endl;
+
+    S = 0;
+    for (k = N; k <= 19; k++)
+    {
+        S += sqrt(pow(sin(k), 2) + pow(cos(N / k), 2));
+    }
+    cout << S << endl;
+
+    S = 0;
+    for (k = 19; k >= N; k--)
+    {
+        S += sqrt(pow(sin(k), 2) + pow(cos(N / k), 2));
+    }
+    cout << S << endl;
+    return 0;
+}
+
